f3107711365fc9c47a1ec5ce2c508c74e54e7830
Mohamed shosha <moshosha267@gmail.com>
2024-08-07 04:06:18 +0300

    Create config package and reformat controller

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -19,174 +19,131 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("Myapp")
 public class EmployeeController {
 
-	// load employee data
+    private final EmployeeService employeeService;
 
-	private EmployeeService employeeService;
-	@Autowired
-	public EmployeeController(EmployeeService employeeService) {
-		super();
-		this.employeeService = employeeService;
-	}
-	@GetMapping("/")
-	public String showhome() {
-		return "home";
-	}
-	@GetMapping("/showMyLoginPage")
-	public String showMyLoginPage() {
-		return "fancy-login";
-	}
-	// add mapping for "/list"
-	@GetMapping("/list")
-	public String listEmployeesForAdmin(Model theModel) {
-		List<Employee> theEmployees = employeeService.findAll();
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
-		return "list-employees";
-	}
-	@GetMapping("/listForEmployee")
-	public String listEmployeesFormToEmployee(Model theModel) {
-		List<Employee> theEmployees = employeeService.findAll();
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
-		return "list-employees-For-Employee";
-	}
-	@GetMapping("/listForManager")
-	public String listEmployeesFormToManager(Model theModel) {
-		List<Employee> theEmployees = employeeService.findAll();
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        super();
+        this.employeeService = employeeService;
+    }
 
-		return "list-employees-For-Manager";
-	}
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
 
-		@GetMapping("/Employees/new")
-		public String createEmployeeForm(Model themodel){
-			Employee Employee = new Employee();
-			themodel.addAttribute("employee" , Employee);
-			return "Employee-form";
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "fancy-login";
+    }
 
-	}
-		@GetMapping("/Employees/newEmployeeForManager")
-		public String createEmployeeFormFromManager(Model themodel){
-			Employee Employee = new Employee();
-			themodel.addAttribute("employee" , Employee);
-			return "Employee-form-from-manager";
+    @GetMapping("/list")
+    public String listEmployeesForAdmin(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees";
+    }
 
-		}
-		@PostMapping("/listForManager")
-		public String saveStudentFromManager(@ModelAttribute("employee") Employee employee){
-		employeeService.save(employee);
-		return "redirect:/listForManager";
-	}
-		@PostMapping("/list")
-		public String saveStudent(@ModelAttribute("employee") Employee employee){
-			employeeService.save(employee);
-			return "redirect:/list";
-	}
-		@GetMapping("/employees/edit/{id}")
-			public String editEmployeeForm (@PathVariable int id , Model model){
-			Employee employeeById = employeeService.findById(id);
-			model.addAttribute("employee",employeeById);
-			return "edit_employee";
-	}
-	@GetMapping("/employees/editForManager/{id}")
-	public String editEmployeeFormForManager (@PathVariable int id , Model model){
-		Employee employeeById = employeeService.findById(id);
-		model.addAttribute("employee",employeeById);
-		return "edit_employee_For-manager";
-	}
+    @GetMapping("/listForEmployee")
+    public String listEmployeesFormToEmployee(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees-For-Employee";
+    }
 
-	@PostMapping("/employeesForManager/{id}")
-	public String updateStudentForManager(@PathVariable int id, @ModelAttribute("employee") Employee employee, Model model) {
+    @GetMapping("/listForManager")
+    public String listEmployeesFormToManager(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees-For-Manager";
+    }
 
-		Employee existingStudent = employeeService.findById(id);
-		existingStudent.setId(id);
-		existingStudent.setFirstName(employee.getFirstName());
-		existingStudent.setLastName(employee.getLastName());
-		existingStudent.setEmail(employee.getEmail());
+    @GetMapping("/Employees/new")
+    public String createEmployeeForm(Model themodel) {
+        Employee Employee = new Employee();
+        themodel.addAttribute("employee", Employee);
+        return "Employee-form";
 
-		employeeService.save(existingStudent);
-		return "redirect:/listForManager";}
-		@PostMapping("/employees/{id}")
-			public String updateStudent(@PathVariable int id, @ModelAttribute("employee") Employee employee, Model model) {
+    }
 
-		Employee existingStudent = employeeService.findById(id);
-			existingStudent.setId(id);
-			existingStudent.setFirstName(employee.getFirstName());
-			existingStudent.setLastName(employee.getLastName());
-			existingStudent.setEmail(employee.getEmail());
+    @GetMapping("/Employees/newEmployeeForManager")
+    public String createEmployeeFormFromManager(Model themodel) {
+        Employee Employee = new Employee();
+        themodel.addAttribute("employee", Employee);
+        return "Employee-form-from-manager";
 
-			employeeService.save(existingStudent);
-			return "redirect:/list";}
-		@GetMapping("/employees/{id}")
-			public String deleteStudent(@PathVariable int id) {
-				employeeService.deleteById(id);
-				return "redirect:/list";
-}
-		@GetMapping("/access-denied")
-		public String showaccessdenied() {
-			return "access-denied";
-		}
-	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-		UserDetails mohamed = User.builder()
-				.username("mohamed")
-				.password("{bcrypt}$2a$12$3J06kgNaw79WCedOqZhq5.JJU/ZqMzlYdi.HDRYDwjg9q.VrehrLe")
-				.roles("EMPLOYEE")
-				.build();
-		UserDetails john = User.builder()
-				.username("ebrahim")
-				.password("{bcrypt}$2a$12$8c994cQFOsgAcbcS35DEnucD6KWdoaCJtScXLaY9VXQk8Z5UUEv3a")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-		UserDetails shosha = User.builder()
-				.username("shosha")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER","ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(mohamed, john, shosha);
+    }
 
-	}
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(configurer ->
-						configurer
-								.requestMatchers( "/listForEmployee").hasRole("EMPLOYEE")
-								.requestMatchers( "/listForEmployee/**").hasRole("EMPLOYEE")
-								.requestMatchers( "/listForManager").hasRole("MANAGER")
-								.requestMatchers( "/listForManager/**").hasRole("MANAGER")
-								.requestMatchers( "/list").hasRole("ADMIN")
-								.anyRequest().authenticated()
-				)
-				.exceptionHandling(configurer ->
-						configurer
-								.accessDeniedPage("/access-denied"))
-				.formLogin(form ->
-						form
-								.loginPage("/showMyLoginPage")
-								.loginProcessingUrl("/authenticateTheUser")
-								.permitAll())
-				.logout( logout -> logout.permitAll());
-		return http.build();
-	}
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests(configurer ->
-//				configurer
-//						.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-//						.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-//						.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-//						.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-//						.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
-//		// use HTTP Basic authentication
-//		http.httpBasic(Customizer.withDefaults());
-//		// disable Cross Site Request Forgery (CSRF)
-//		http.csrf(csrf -> csrf.disable());
-//		return http.build(
-//	}
+    @PostMapping("/listForManager")
+    public String saveStudentFromManager(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/listForManager";
+    }
+
+    @PostMapping("/list")
+    public String saveStudent(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/employees/edit/{id}")
+    public String editEmployeeForm(@PathVariable int id, Model model) {
+        Employee employeeById = employeeService.findById(id);
+        model.addAttribute("employee", employeeById);
+        return "edit_employee";
+    }
+
+    @GetMapping("/employees/editForManager/{id}")
+    public String editEmployeeFormForManager(@PathVariable int id, Model model) {
+        Employee employeeById = employeeService.findById(id);
+        model.addAttribute("employee", employeeById);
+        return "edit_employee_For-manager";
+    }
+
+    @PostMapping("/employeesForManager/{id}")
+    public String updateStudentForManager(@PathVariable int id, @ModelAttribute("employee") Employee employee, Model model) {
+        Employee existingStudent = employeeService.findById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(employee.getFirstName());
+        existingStudent.setLastName(employee.getLastName());
+        existingStudent.setEmail(employee.getEmail());
+        employeeService.save(existingStudent);
+        return "redirect:/listForManager";
+    }
+
+    @PostMapping("/employees/{id}")
+    public String updateStudent(@PathVariable int id, @ModelAttribute("employee") Employee employee, Model model) {
+
+        Employee existingStudent = employeeService.findById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(employee.getFirstName());
+        existingStudent.setLastName(employee.getLastName());
+        existingStudent.setEmail(employee.getEmail());
+
+        employeeService.save(existingStudent);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/employees/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        employeeService.deleteById(id);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
+    }
+
+
+
+
 }
 
 
--- src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -12,43 +12,38 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-	private EmployeeRepository employeeRepository;
-	
-	@Autowired
-	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
-		employeeRepository = theEmployeeRepository;
-	}
-	
-	@Override
-	public List<Employee> findAll() {
-		return employeeRepository.findAll();
-	}
+    private final EmployeeRepository employeeRepository;
 
-	@Override
-	public Employee findById(int theId) {
-		Optional<Employee> result = employeeRepository.findById(theId);
-		
-		Employee theEmployee = null;
-		
-		if (result.isPresent()) {
-			theEmployee = result.get();
-		}
-		else {
-			// we didn't find the employee
-			throw new RuntimeException("Did not find employee id - " + theId);
-		}
-		
-		return theEmployee;
-	}
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
 
-	@Override
-	public void save(Employee theEmployee) {
-		employeeRepository.save(theEmployee);
-	}
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
 
-	@Override
-	public void deleteById(int theId) {
-		employeeRepository.deleteById(theId);
-	}
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+        return theEmployee;
+    }
 
+    @Override
+    public void save(Employee theEmployee) {
+        employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
 }
